a58ae12724dfec826e63d0d22427afa01c528e3c
Mrudang Patel <75343041+myp42033@users.noreply.github.com>
2024-04-10 08:23:03 -0400

    added profile component to homepage

--- nametbd/frontend/src/components/homepage.js
+++ nametbd/frontend/src/components/homepage.js
@@ -1,6 +1,7 @@
 import '../styles/homepage.css';
 import Post from './post.js';
 import Navbar from './navbar.js';
+import Profile from './profile.js';
 
 export default function homepage() {
 
@@ -58,6 +59,7 @@ export default function homepage() {
   return (
     <div className="home">
       <Navbar></Navbar>
+      <Profile></Profile>
       <div className="Page">
         {posts.map((posts, index) => {
           return <Post className = "post" item={posts} />;
