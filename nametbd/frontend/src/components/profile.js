f59ff91ad845528deeb3ad744758b8883c33c6bb
Mrudang Patel <75343041+myp42033@users.noreply.github.com>
2024-04-10 13:22:47 -0400

    Improving styling for profile page

--- nametbd/frontend/src/components/profile.js
+++ nametbd/frontend/src/components/profile.js
@@ -1,37 +1,41 @@
-import '../styles/profile.css';
 import React from 'react';
+import Navbar from './navbar.js';
+import '../styles/profile.css';
+import '../styles/navbar.css';
 
 function profile() {
 
     return (
-        <div className="userProfile">
-            <div className="profileHeader">
-                <h2>USER PROFILE</h2>
-                <button className="editProfileButton">Edit Profile</button>
+        <div className="profilePage">
+            <Navbar></Navbar>
+            <div className="userProfile">
+                <div className="profileHeader">
+                    <div className="profilePicture">
+                        <img src="https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg" alt="User/Profile Picture"/>
+                    </div>
+                    <h2>user-name</h2>
+                    <button className="editProfileButton">Edit Profile</button>
+                </div>
+
+                <div className="profileInfo">
+                    <div className="userDetails">
+                        <p>Username: Mrudang Patel</p>
+                        <p>Email: myp2k2nd@gmail.com</p>
+                        <p>Number of posts: 0</p>
+                        <p>Member since: 04-09-2024</p>
+                        <p>Bio: Something blah blah blah...</p>
+                    </div>
+
+                    <div className="Posts">
+                        <button className="createPostButton">Create Post</button>
+                        <h3>My Posts:</h3>
+
+                    </div>
+
+                </div>
+
+                <button className = "shareProfileButton">Share Profile</button>
             </div>
-
-            <div className="profileInfo">
-                <div className="profilePicture">
-                    <img src="https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg" alt="User/Profile Picture"/>
-                </div>
-
-                <div className="userDetails">
-                    <p>Username: Mrudang Patel</p>
-                    <p>Email: myp2k2nd@gmail.com</p>
-                    <p>Number of posts: 0</p>
-                    <p>Member since: 04-09-2024</p>
-                    <p>Bio: Something blah blah blah...</p>
-                </div>
-
-                <div className="Posts">
-                    <button className="createPostButton">Create Post</button>
-                    <h3>My Posts:</h3>
-
-                </div>
-
-            </div>
-
-            <button className = "shareProfileButton">Share Profile</button>
         </div>
     );
 
