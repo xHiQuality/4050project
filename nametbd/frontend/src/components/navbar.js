ad2d13cc99bda79d24665624e2a339991bcc5da5
amilyc16 <99252644+amilyc16@users.noreply.github.com>
2024-04-11 21:03:43 -0400

    inital log in page - frontend

--- nametbd/frontend/src/components/navbar.js
+++ nametbd/frontend/src/components/navbar.js
@@ -1,54 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/navbar.css';
+import Authmodal from './Authmodal.js';
 
-function navbar(props) {
-    
+function Navbar(props) {
+  // State to manage the visibility of the Authmodel
+  const [showAuthModal, setShowAuthModal] = useState(false);
 
-return (
-  <div className = "container">
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <button className = "logo-button">
-        <img
-          src="https://1000logos.net/wp-content/uploads/2017/05/Reddit-Logo.png"
-          alt="not-reddit-logo"
-          className="logo"
-        />
-        </button>
-      </div>
-      <div className="bar">
-        <input
-          type="text"
-          placeholder="Search Not Reddit"
-          id="navbar-search-field"
-        />
-        <img
-          src="https://icon-library.com/images/search-icon-png-transparent/search-icon-png-transparent-18.jpg"
-          alt="search icon"
-          className="search-icon"
-          sytle={{ cursor: 'pointer' }}
-        />
-      </div>
-      <button className = "navButton" id="message-button">
-        <img
+  // Function to toggle the visibility of the Authmodel
+  const toggleAuthModal = () => {
+    setShowAuthModal(!showAuthModal);
+  };
+
+  return (
+    <div className="container">
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <button className="logo-button">
+            <img
+              src="https://1000logos.net/wp-content/uploads/2017/05/Reddit-Logo.png"
+              alt="not-reddit-logo"
+              className="logo"
+            />
+          </button>
+        </div>
+        <div className="bar">
+          <input
+            type="text"
+            placeholder="Search Not Reddit"
+            id="navbar-search-field"
+          />
+          <img
+            src="https://icon-library.com/images/search-icon-png-transparent/search-icon-png-transparent-18.jpg"
+            alt="search icon"
+            className="search-icon"
+            style={{ cursor: 'pointer' }}
+          />
+        </div>
+        <button className="navButton" id="message-button">
+          <img
             src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/3216841/chat-circle-dots-icon-md.png"
             alt="message box"
             className="message-icon"
-            sytle={{ cursor: 'pointer' }}
+            style={{ cursor: 'pointer' }}
           />
-      </button>
-      <button className = "navButton" id="create-button">
+        </button>
+        <button className="navButton" id="create-button">
           <img
-              src="https://www.freepnglogos.com/uploads/plus-icon/plus-icon-plus-math-icon-download-icons-9.png"
-              alt="create post"
-              className="plus-icon"
-              sytle={{ cursor: 'pointer' }}
+            src="https://www.freepnglogos.com/uploads/plus-icon/plus-icon-plus-math-icon-download-icons-9.png"
+            alt="create post"
+            className="plus-icon"
+            style={{ cursor: 'pointer' }}
           />
-      </button>
-        <button className = "navButton" id="login-button">Login</button>
-        </nav>
-        <hr></hr>
-        </div>
-)};
+        </button>
+        
+        {/* Button to toggle the Authmodel */}
+        <button className="navButton" id="login-button" onClick={toggleAuthModal}>
+          Login
+        </button>
+      </nav>
+      <hr />
+      
+      {/* Render Authmodel conditionally */}
+      {showAuthModal && <Authmodal />}
+    </div>
+  );
+}
 
-export default navbar;
+export default Navbar;
