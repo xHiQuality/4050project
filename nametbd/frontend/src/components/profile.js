3925a12bc4682f1f79807a5574f6d354d669b780
Mrudang Patel <75343041+myp42033@users.noreply.github.com>
2024-04-21 23:41:22 -0400

    profile functionality

--- nametbd/frontend/src/components/profile.js
+++ nametbd/frontend/src/components/profile.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Navbar from './navbar.js';
 import '../styles/profile.css';
 import '../styles/navbar.css';
+import axios from 'axios';
+import Post from  './post.js';
+import homepage from './homepage.js';
 
-function profile() {
+function Profile() {
+    const [userData, setUserData] = useState({});
+    const [userPosts, setUserPosts] = useState([]);
+
+    useEffect (() => {
+        getUserData();
+        getUserPosts();
+    }, []);
+
+    const getUserData = async(username) => {
+        try {
+            const response = await axios.get(`/api/posts?author=${username}`);
+            const data = response.json();
+            setUserData(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const getUserPosts = async(username) => {
+        try {
+            const response = await axios.get(`/api/posts?author=${username}`);
+            const data = response.json();
+            setUserPosts(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
         <div className="profilePage">
@@ -13,7 +43,7 @@ function profile() {
                     <div className="profilePicture">
                         <img src="https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg" width = "150" height = "150px" alt="User/Profile Picture"/>
                     </div>
-                    <h2>user-name</h2>
+                    <h2>{userData.username}</h2>
                 </div>
                 
                 <button className ="editProfileButton">Edit Profile</button>
@@ -22,17 +52,20 @@ function profile() {
 
                 <div className="profileInfo">  
                     <div className="userDetails">
-                        <p>Username: Mrudang Patel</p>
-                        <p>Email: myp2k2nd@gmail.com</p>
-                        <p>Number of posts: 0</p>
-                        <p>Member since: 04-09-2024</p>
-                        <p>Bio: Something blah blah blah...</p>
+                        <p>Username: {userData.username}</p>
+                        <p>Email: ///</p>
+                        <p>Number of posts: ///</p>
+                        <p>Member since: ///</p>
+                        <p>Bio: ///</p>
                     </div>
 
                     <div className="Posts">
                         <button className="createPostButton">Create Post</button>
                         <h3>My Posts:</h3>
-
+                        {userPosts.map((posts, index) => (
+                            <Post className = "posts" key={index} item={posts}/>
+                        ))}
+                        
                     </div>
 
                 </div>
@@ -43,4 +76,4 @@ function profile() {
 
 }
 
-export default profile;
+export default Profile; 
